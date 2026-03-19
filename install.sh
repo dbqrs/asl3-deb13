52ea8399bd66a59803352d6639604d91c40f6750
dbqrs <58705854+dbqrs@users.noreply.github.com>
2026-03-19 12:52:59 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ cd /tmp
 wget https://repo.allstarlink.org/public/asl-apt-repos.deb13_all.deb
 sudo dpkg -i asl-apt-repos.deb13_all.deb
 sudo apt update
-sudo apt install asl3-appliance-pc
+sudo apt install asl3-appliance-pc -y
