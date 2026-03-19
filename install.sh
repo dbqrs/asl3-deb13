1bb88d6118902bdd9ccb9783c6c7bbec5586b782
dbqrs <58705854+dbqrs@users.noreply.github.com>
2026-03-19 12:13:58 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+cd /tmp
+wget https://repo.allstarlink.org/public/asl-apt-repos.deb13_all.deb
+sudo dpkg -i asl-apt-repos.deb13_all.deb
+sudo apt update
+sudo apt install asl3-appliance-pc
